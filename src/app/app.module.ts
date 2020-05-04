6ef923962571ad992f8b8400bc62f05ab8dab514
lfelipeblopes <52787612+lfelipeblopes@users.noreply.github.com>
2020-05-04 17:02:01 -0300

    mygames

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { NewComponent } from './new/new.component';
+import { E404Component } from './e404/e404.component';
+import { AboutComponent } from './about/about.component';
+import { FormComponent } from './form/form.component';
+
+// Importa componentes de formulário
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent,
+    NewComponent,
+    E404Component,
+    AboutComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // Inicialização do Firebase
+    AngularFireModule.initializeApp(environment.firebase),
+
+    // Carga da classe de acesso ao Firestore
+    AngularFirestoreModule,
+
+    // Carga de FormsModule
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDujCI6Wqy5pgZh_rCJZ0SA2zZMnzlBy-s',
+    authDomain: 'mygamesangular.firebaseapp.com',
+    databaseURL: 'https://mygamesangular.firebaseio.com',
+    projectId: 'mygamesangular',
+    storageBucket: 'mygamesangular.appspot.com',
+    messagingSenderId: '885848718029',
+    appId: '1:885848718029:web:6b698b97bf95b61d84c784'
+  }
 };
 
 /*
