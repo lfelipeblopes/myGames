4d16efb8b307cee05cc7296d5975c4396baf1012
lfelipeblopes <52787612+lfelipeblopes@users.noreply.github.com>
2020-05-04 17:05:33 -0300

    mygames

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDujCI6Wqy5pgZh_rCJZ0SA2zZMnzlBy-s',
-    authDomain: 'mygamesangular.firebaseapp.com',
-    databaseURL: 'https://mygamesangular.firebaseio.com',
-    projectId: 'mygamesangular',
-    storageBucket: 'mygamesangular.appspot.com',
-    messagingSenderId: '885848718029',
-    appId: '1:885848718029:web:6b698b97bf95b61d84c784'
+    apiKey: "AIzaSyCFHgJvX1nyQUooNdAho4GkyXAdM1VUKYs",
+    authDomain: "mygames-3d020.firebaseapp.com",
+    databaseURL: "https://mygames-3d020.firebaseio.com",
+    projectId: "mygames-3d020",
+    storageBucket: "mygames-3d020.appspot.com",
+    messagingSenderId: "841196857403",
+    appId: "1:841196857403:web:0f15f2cd7f443e574fd41e"
   }
 };
